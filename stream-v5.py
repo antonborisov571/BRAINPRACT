0d16edd95a3e88304ac04081d7fb567e4d7292a3
Anton Borisov <Antonborisov571@gmail.com>
2023-07-11 18:37:51 +0300

    Added code for yolov5 and yolov8

---
+++ stream-v5.py
@@ -0,0 +1,22 @@
+import cv2
+import torch
+
+model = torch.hub.load("ultralytics/yolov5", 'custom', path='yolov5.pt')
+model.conf = 0.5
+
+cap = cv2.VideoCapture(0)
+cv2.namedWindow("YOLOv5 Inference", cv2.WINDOW_NORMAL)
+
+while True:
+    success, frame = cap.read()
+    if success:
+        results = model(frame)
+        results.render()
+        cv2.imshow("YOLOv5 Inference", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        break
+cap.release()
+cv2.destroyAllWindows()
---
+++ stream-v8.py
@@ -0,0 +1,23 @@
+import cv2
+import torch
+from ultralytics import YOLO
+
+model = YOLO('yolov8.pt')
+model.conf = 0.5
+
+cap = cv2.VideoCapture(0)
+cv2.namedWindow("YOLOv8 Inference", cv2.WINDOW_NORMAL)
+
+while cap.isOpened():
+    success, frame = cap.read()
+    if success:
+        results = model.predict(frame, conf=0.5)
+        annotated_frame = results[0].plot()
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        break
+cap.release()
+cv2.destroyAllWindows()
